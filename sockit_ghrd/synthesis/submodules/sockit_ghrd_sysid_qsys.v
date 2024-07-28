49b37943d2c795d91c9410c67074e996aec6e93a
zhonghuashu <77599567@qq.com>
2024-07-28 12:43:35 +0800

    Upgrade to 20.1

--- sockit_ghrd/synthesis/submodules/sockit_ghrd_sysid_qsys.v
+++ sockit_ghrd/synthesis/submodules/sockit_ghrd_sysid_qsys.v
@@ -1,4 +1,4 @@
-// (C) 2001-2019 Intel Corporation. All rights reserved.
+// (C) 2001-2020 Intel Corporation. All rights reserved.
 // Your use of Intel Corporation's design tools, logic functions and other 
 // software and tools, and its AMPP partner logic functions, and any output 
 // files from any of the foregoing (including device programming or simulation 
@@ -49,7 +49,7 @@ module sockit_ghrd_sysid_qsys (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1574126495 : 5584;
+  assign readdata = address ? 1722139952 : 5584;
 
 endmodule
 
